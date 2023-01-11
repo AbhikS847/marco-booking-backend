24fcdc7e03f4782f6cbada499e31b246af2ab0d8
AbhikS847 <abhikshrestha@gmail.com>
2023-01-12 01:15:40 +1100

    Added cors for all access

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('../middleware/errorMiddleware');
 const connectDB = require('../config/db');
+const cors = require('cors');
 
 connectDB();
 
@@ -9,6 +10,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+app.use(cors({
+    origin:'*'
+}))
 
 app.use('/booking', require('../routes/bookingRoutes'));
 
